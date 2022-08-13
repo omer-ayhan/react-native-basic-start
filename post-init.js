3e3fbeb5fff02aa70721fbc530fa2efa9fce5ad4
omer-ayhan <om.ayhan247@gmail.com>
2022-08-13 21:58:13 +0300

    fix: post script

--- post-init.js
+++ post-init.js
@@ -1,13 +1,17 @@
 const { exec } = require("child_process");
 const fs = require("fs");
+const path = require("path");
 
-exec("npx pod-install ios").on("error", (err) => {
-	console.log(err);
+exec("yarn install").on("close", () => {
+	exec("npx pod-install ios").on("error", (err) => {
+		console.log(err);
+	});
 });
 
 const renameEnvArr = ["_env", "_env.production", "_env.development"];
 
-renameEnvArr.forEach((filePath) => {
+renameEnvArr.forEach((file) => {
+	const filePath = path.join(__dirname, file);
 	const exists = fs.existsSync(filePath);
 	if (exists) {
 		fs.renameSync(filePath, filePath.replace("_env", ".env"));
